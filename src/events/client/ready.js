2ceef44ebc71fdd420730bffab2c3d8fa3fcde4c
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-11 01:19:49 +0600

    Delete src directory

--- src/events/client/ready.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: "ready",
-  once: true,
-  execute(client) {
-    console.log(`Logged in as ${client.user.tag}`);
-    client.afkMap = new Map(); // Initialize the AFK map to handle AFK users
-  },
-};
--- src/events/guild/messageDelete.js
+++
@@ -1,9 +0,0 @@
-import { logMessageDelete } from "../../utils/logging.js";
-
-export default {
-  name: "messageDelete",
-  async execute(message, client) {
-    if (message.partial) await message.fetch();
-    logMessageDelete(client, message);
-  },
-};
