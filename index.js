8f909432c1c3c0af9e682e23115a1640ec1e04dc
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-11 03:08:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const { loadCommands, loadEvents } = require('./src/utils/loader');
 const mongoose = require('mongoose');
-require('dotenv').config(); // Loads environment variables from .env
+require('dotenv').config();
+
+mongoose.set('strictQuery', true);  // This line suppresses the deprecation warning
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildVoiceStates
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates,
     ]
 });
 
-client.commands = new Collection();
-client.buttons = new Collection();  // To manage button interactions
 client.slashCommands = new Collection();
 
-// Load commands and events from the utils/loader.js file
+// Load commands and events
+const { loadCommands, loadEvents } = require('./src/utils/loader');
 loadCommands(client);
 loadEvents(client);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 }).then(() => console.log('Connected to MongoDB')).catch(console.error);
 
-// Bot login
-client.login(process.env.TOKEN);  // Using the TOKEN from the .env file
+// Login bot
+client.login(process.env.TOKEN);
