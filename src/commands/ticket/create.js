4fd5697024235b2326b779fe5e53bbaf0b25b23e
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-11 03:04:03 +0600

    Update create.js

--- src/commands/ticket/create.js
+++ src/commands/ticket/create.js
@@ -1,10 +1,19 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
 const Ticket = require('../../models/Ticket');
+const { handleTicketCreate } = require('../../utils/ticketHandler');
 
 module.exports = {
     name: 'create-ticket',
-    description: 'Create a support ticket',
+    description: 'Create a new support ticket',
     async execute(interaction) {
+        const userId = interaction.user.id;
+
+        // Check if the user already has an open ticket
+        const existingTicket = await Ticket.findOne({ userId, status: 'open' });
+        if (existingTicket) {
+            return interaction.reply({ content: 'You already have an open ticket.', ephemeral: true });
+        }
+
         const embed = new EmbedBuilder()
             .setTitle('Support Ticket')
             .setDescription('Click the button below to create a ticket.')
@@ -17,6 +26,9 @@ module.exports = {
                 .setStyle('PRIMARY')
         );
 
-        await interaction.reply({ embeds: [embed], components: [row] });
+        await interaction.reply({ embeds: [embed], components: [row], ephemeral: true });
+
+        // Handle the ticket creation in the background
+        handleTicketCreate(interaction);
     },
 };
