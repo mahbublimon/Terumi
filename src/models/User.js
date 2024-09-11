fd8a6f09fddd5be8b6b8c138433c1eb909b20bdb
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-11 12:07:04 +0600

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,12 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-  userID: { type: String, required: true },
-  guildID: { type: String, required: true },
-  experience: { type: Number, default: 0 },
-  level: { type: Number, default: 1 },
+  userID: { type: String, required: true }, // Discord User ID
+  guildID: { type: String, required: true }, // Guild ID for server-specific leveling
+  experience: { type: Number, default: 0 }, // XP points
+  level: { type: Number, default: 1 },      // Level of the user
+  reputation: { type: Number, default: 0 }, // Reputation points
+  lastMessage: { type: Date, default: Date.now } // To prevent XP farming
 });
 
 module.exports = model('User', userSchema);
