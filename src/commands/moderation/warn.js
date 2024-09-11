b78a9d9bffec08e7bf6032e377e43135b80888c6
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-11 20:17:42 +0600

    Update warn.js

--- src/commands/moderation/warn.js
+++ src/commands/moderation/warn.js
@@ -1,4 +1,6 @@
 // src/commands/moderation/warn.js
+const hasAdminPermissions = require('../../utils/permissionCheck');
+
 module.exports = {
   data: {
     name: 'warn',
@@ -7,30 +9,25 @@ module.exports = {
       {
         name: 'user',
         type: 'USER',
-        description: 'The user to warn',
+        description: 'User to warn',
         required: true,
       },
       {
         name: 'reason',
         type: 'STRING',
-        description: 'The reason for the warning',
+        description: 'Reason for warning',
         required: true,
       },
     ],
   },
   async execute(interaction) {
-    const user = interaction.options.getUser('user');
-    const reason = interaction.options.getString('reason');
-    
-    if (!interaction.member.permissions.has('MODERATE_MEMBERS')) {
-      return interaction.reply('You do not have permission to use this command.');
+    if (!hasAdminPermissions(interaction.member)) {
+      return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
     }
 
-    try {
-      await user.send(`You have been warned for: ${reason}`);
-      await interaction.reply(`${user.tag} has been warned for: ${reason}`);
-    } catch (error) {
-      await interaction.reply('Failed to warn the user. They may have DMs disabled.');
-    }
+    const user = interaction.options.getUser('user');
+    const reason = interaction.options.getString('reason');
+    await user.send(`You have been warned for the following reason: ${reason}`);
+    await interaction.reply(`Warned ${user.username} for: ${reason}`);
   },
 };
