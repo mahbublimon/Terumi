0dcc1207ccbe18ebd1ad8fae172e2cb7490fe1bb
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-11 20:18:36 +0600

    Update kick.js

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,28 +1,35 @@
+// src/commands/moderation/kick.js
+const hasAdminPermissions = require('../../utils/permissionCheck');
+
 module.exports = {
+  data: {
     name: 'kick',
     description: 'Kick a user from the server',
     options: [
-        {
-            name: 'user',
-            type: 'USER',
-            description: 'The user to kick',
-            required: true
-        },
-        {
-            name: 'reason',
-            type: 'STRING',
-            description: 'Reason for the kick',
-            required: false
-        }
+      {
+        name: 'user',
+        type: 'USER',
+        description: 'User to kick',
+        required: true,
+      },
+      {
+        name: 'reason',
+        type: 'STRING',
+        description: 'Reason for kicking the user',
+        required: true,
+      },
     ],
-    async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        const reason = interaction.options.getString('reason') || 'No reason provided';
-
-        const member = await interaction.guild.members.fetch(user.id);
-        if (!member.kickable) return interaction.reply('I cannot kick this user.');
-
-        await member.kick(reason);
-        await interaction.reply(`${user.username} has been kicked for: ${reason}`);
+  },
+  async execute(interaction) {
+    if (!hasAdminPermissions(interaction.member)) {
+      return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
     }
+
+    const user = interaction.options.getUser('user');
+    const reason = interaction.options.getString('reason');
+    const member = interaction.guild.members.cache.get(user.id);
+
+    await member.kick(reason);
+    await interaction.reply(`${user.username} has been kicked. Reason: ${reason}`);
+  },
 };
