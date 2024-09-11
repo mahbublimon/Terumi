a975c75336d1e3e048d1d4099bc853874d4986a3
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-11 12:14:16 +0600

    Update hug.js

--- src/commands/fun/hug.js
+++ src/commands/fun/hug.js
@@ -1,16 +1,30 @@
+// src/commands/fun/hug.js
+const axios = require('axios');
+
 module.exports = {
+  data: {
     name: 'hug',
-    description: 'Hug a user!',
+    description: 'Hug someone',
     options: [
-        {
-            name: 'user',
-            type: 'USER',
-            description: 'The user to hug',
-            required: true,
-        }
+      {
+        name: 'user',
+        type: 'USER',
+        description: 'User to hug',
+        required: true,
+      },
     ],
-    async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        await interaction.reply(`${interaction.user.username} hugged ${user.username}!`);
-    }
+  },
+  async execute(interaction) {
+    const target = interaction.options.getUser('user');
+    const gifResponse = await axios.get(`https://api.giphy.com/v1/gifs/random`, {
+      params: {
+        api_key: process.env.GIPHY_SDK,
+        tag: 'hug',
+      },
+    });
+
+    const gifUrl = gifResponse.data.data.images.original.url;
+
+    await interaction.reply(`${interaction.user.username} hugged ${target.username}! ${gifUrl}`);
+  },
 };
