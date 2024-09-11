179b91b757da9d922783c200e53ce28611499e98
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-11 23:47:08 +0600

    Create lyrics.js

---
+++ src/commands/music/lyrics.js
@@ -0,0 +1,24 @@
+// src/commands/music/lyrics.js
+const lyricsFinder = require('lyrics-finder');
+const player = require('../../utils/musicPlayer');
+
+module.exports = {
+  data: {
+    name: 'lyrics',
+    description: 'Get the lyrics for the current song',
+  },
+  async execute(interaction) {
+    const queue = player.getQueue(interaction.guild.id);
+
+    if (!queue || !queue.playing) {
+      return interaction.reply({ content: 'There is no music playing!', ephemeral: true });
+    }
+
+    const currentTrack = queue.current;
+    const lyrics = await lyricsFinder(currentTrack.author, currentTrack.title) || 'No lyrics found!';
+
+    return interaction.reply({
+      content: `**Lyrics for ${currentTrack.title}:**\n${lyrics}`,
+    });
+  },
+};
