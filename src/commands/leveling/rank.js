3be51e94d0c7dc9524c1808a9503d6585ddcd098
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-11 12:08:17 +0600

    Update rank.js

--- src/commands/leveling/rank.js
+++ src/commands/leveling/rank.js
@@ -1,20 +1,35 @@
 // src/commands/leveling/rank.js
 const User = require('../../models/User');
 const { EmbedBuilder } = require('discord.js');
+const { getRequiredXP } = require('../../utils/leveling');
 
 module.exports = {
   data: {
     name: 'rank',
-    description: 'Shows your current rank',
+    description: 'Shows your current rank on the server',
+    options: [
+      {
+        name: 'user',
+        type: 'USER',
+        description: 'User to show the rank of',
+        required: false,
+      },
+    ],
   },
   async execute(interaction) {
-    const user = await User.findOne({ userID: interaction.user.id, guildID: interaction.guild.id });
-    if (!user) return interaction.reply('You have no rank yet.');
+    const target = interaction.options.getUser('user') || interaction.user;
+    const user = await User.findOne({ userID: target.id, guildID: interaction.guild.id });
 
+    if (!user) return interaction.reply(`${target.username} has no rank yet.`);
+
+    const requiredXP = getRequiredXP(user.level);
     const embed = new EmbedBuilder()
-      .setTitle(`${interaction.user.username}'s Rank`)
-      .addFields({ name: 'Level', value: user.level.toString() })
-      .addFields({ name: 'Experience', value: user.experience.toString() })
+      .setTitle(`${target.username}'s Rank`)
+      .addFields(
+        { name: 'Level', value: user.level.toString(), inline: true },
+        { name: 'Experience', value: `${user.experience}/${requiredXP} XP`, inline: true }
+      )
+      .setThumbnail(target.displayAvatarURL({ dynamic: true }))
       .setColor('#FFD700');
 
     await interaction.reply({ embeds: [embed] });
