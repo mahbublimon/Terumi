2d5d08761d2baa10617e18eadd220ea930416344
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-11 12:02:14 +0600

    Create musicPlayer.js

---
+++ src/commands/utility/musicPlayer.js
@@ -0,0 +1,23 @@
+// src/utils/musicPlayer.js
+const { DisTube } = require('@distube/disnake');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = (client) => {
+  const distube = new DisTube(client, { emitNewSongOnly: true });
+
+  client.distube = distube;
+
+  client.distube
+    .on('playSong', (queue, song) => {
+      const embed = new EmbedBuilder()
+        .setTitle('Now Playing')
+        .setDescription(`[${song.name}](${song.url}) - \`${song.formattedDuration}\``);
+      queue.textChannel.send({ embeds: [embed] });
+    })
+    .on('addSong', (queue, song) => {
+      const embed = new EmbedBuilder()
+        .setTitle('Added to Queue')
+        .setDescription(`[${song.name}](${song.url}) - \`${song.formattedDuration}\``);
+      queue.textChannel.send({ embeds: [embed] });
+    });
+};
