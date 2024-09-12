8fefc34705c4526136d47487eee7954a060d3527
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-13 02:53:10 +0600

    Update setlevelroles.js

--- src/commands/admin/setlevelroles.js
+++ src/commands/admin/setlevelroles.js
@@ -1,31 +1,23 @@
-// src/commands/admin/setlevelroles.js
+const { SlashCommandBuilder } = require('discord.js');
 const LevelRoles = require('../../models/LevelRoles');
 
 module.exports = {
-  data: {
-    name: 'setlevelroles',
-    description: 'Set role rewards for reaching specific levels',
-    options: [
-      {
-        name: 'level',
-        type: 'INTEGER',
-        description: 'The level at which the role is assigned',
-        required: true,
-      },
-      {
-        name: 'role',
-        type: 'ROLE',
-        description: 'The role to be assigned at that level',
-        required: true,
-      },
-      {
-        name: 'stack_roles',
-        type: 'BOOLEAN',
-        description: 'Should roles stack (true) or not (false)',
-        required: false,
-      },
-    ],
-  },
+  data: new SlashCommandBuilder()
+    .setName('setlevelroles')
+    .setDescription('Set role rewards for reaching specific levels')
+    .addIntegerOption(option =>
+      option.setName('level')
+        .setDescription('The level at which the role is assigned')
+        .setRequired(true))
+    .addRoleOption(option =>
+      option.setName('role')
+        .setDescription('The role to be assigned at that level')
+        .setRequired(true))
+    .addBooleanOption(option =>
+      option.setName('stack_roles')
+        .setDescription('Should roles stack (true) or not (false)')
+        .setRequired(false)),
+
   async execute(interaction) {
     if (!interaction.member.permissions.has('ADMINISTRATOR')) {
       return interaction.reply({ content: 'You must be an administrator to use this command.', ephemeral: true });
