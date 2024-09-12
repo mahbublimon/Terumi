ce924c185b259000e68c1ef9734bd438098a4457
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-13 04:29:22 +0600

    Update prune.js

--- src/commands/moderation/prune.js
+++ src/commands/moderation/prune.js
@@ -1,19 +1,15 @@
-// src/commands/moderation/prune.js
+const { SlashCommandBuilder } = require('discord.js');
 const hasAdminPermissions = require('../../utils/permissionCheck');
 
 module.exports = {
-  data: {
-    name: 'prune',
-    description: 'Delete a specified number of messages',
-    options: [
-      {
-        name: 'amount',
-        type: 'INTEGER',
-        description: 'Number of messages to delete',
-        required: true,
-      },
-    ],
-  },
+  data: new SlashCommandBuilder()
+    .setName('prune')
+    .setDescription('Delete a specified number of messages')
+    .addIntegerOption(option =>
+      option.setName('amount')
+        .setDescription('Number of messages to delete')
+        .setRequired(true)),
+
   async execute(interaction) {
     if (!hasAdminPermissions(interaction.member)) {
       return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
