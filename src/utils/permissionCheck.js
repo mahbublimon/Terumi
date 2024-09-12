d3781eb60710c7d943ed8ad847d4d18b4faf1001
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-13 04:35:46 +0600

    Update permissionCheck.js

--- src/utils/permissionCheck.js
+++ src/utils/permissionCheck.js
@@ -1,11 +1,12 @@
-// src/utils/permissionCheck.js
 const ModerationRoles = require('../models/ModerationRoles');
 
 async function hasModerationPermissions(member) {
+  // Grant permission if the member is an administrator
   if (member.permissions.has('ADMINISTRATOR')) return true;
 
+  // Fetch the roles that have moderation permissions
   const moderationRoles = await ModerationRoles.findOne({ guildID: member.guild.id });
-  if (!moderationRoles) return false; // No roles set, deny permission
+  if (!moderationRoles) return false; // If no moderation roles are set, deny permission
 
   // Check if the member has any of the allowed roles
   return member.roles.cache.some(role => moderationRoles.roles.includes(role.id));
