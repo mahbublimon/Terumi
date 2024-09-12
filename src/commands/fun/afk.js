803c4c28c1815eaeacc12a825d64974904be7fd3
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-12 23:15:27 +0600

    Update afk.js

--- src/commands/fun/afk.js
+++ src/commands/fun/afk.js
@@ -1,40 +1,25 @@
-// src/commands/fun/afk.js
+const { SlashCommandBuilder } = require('discord.js');
+
+// A map to store AFK users and their reason
 const afkUsers = new Map();
 
 module.exports = {
-  data: {
-    name: 'afk',
-    description: 'Set your AFK status',
-    options: [
-      {
-        name: 'reason',
-        type: 'STRING',
-        description: 'Reason for being AFK',
-        required: false,
-      },
-    ],
-  },
+  data: new SlashCommandBuilder()
+    .setName('afk')
+    .setDescription('Set your AFK status')
+    .addStringOption(option =>
+      option.setName('reason')
+        .setDescription('Reason for being AFK')
+        .setRequired(false)
+    ),
+
   async execute(interaction) {
     const reason = interaction.options.getString('reason') || 'AFK';
     afkUsers.set(interaction.user.id, { reason, timestamp: Date.now() });
 
     await interaction.reply(`${interaction.user.username} is now AFK: ${reason}`);
   },
-};
-
-// Check for mentions in messageCreate event
-module.exports = async (client, message) => {
-  if (message.mentions.users.size > 0) {
-    message.mentions.users.forEach((user) => {
-      if (afkUsers.has(user.id)) {
-        const afkData = afkUsers.get(user.id);
-        message.reply(`${user.username} is currently AFK: ${afkData.reason}`);
-      }
-    });
-  }
-
-  if (afkUsers.has(message.author.id)) {
-    afkUsers.delete(message.author.id);
-    message.reply(`Welcome back, ${message.author.username}. AFK status removed.`);
-  }
+  
+  // Export the AFK users map to be used in messageCreate event
+  afkUsers
 };
