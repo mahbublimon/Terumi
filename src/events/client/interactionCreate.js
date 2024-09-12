d289f24646e01b23d398772adb951ca23e26bc5f
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-12 10:09:35 +0600

    Update interactionCreate.js

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -1,21 +1,10 @@
-module.exports = {
-    name: 'interactionCreate',
-    async execute(interaction, client) {
-        if (interaction.isCommand()) {
-            const command = client.slashCommands.get(interaction.commandName);
-            if (!command) return;
+// src/events/client/interactionCreate.js
+const ticketHandler = require('../../utils/ticketHandler');
 
-            try {
-                await command.execute(interaction, client);
-            } catch (error) {
-                console.error(error);
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }
-        } else if (interaction.isButton()) {
-            if (interaction.customId === 'create_ticket') {
-                const { handleTicketCreate } = require('../../utils/ticketHandler');
-                handleTicketCreate(interaction);
-            }
-        }
-    },
+module.exports = async (client, interaction) => {
+  if (!interaction.isButton()) return;
+
+  if (interaction.customId === 'create_ticket') {
+    await ticketHandler.createTicket(interaction, interaction.user);
+  }
 };
