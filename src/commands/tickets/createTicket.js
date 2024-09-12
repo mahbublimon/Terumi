7fa4a7e8ecadd51755ea9b94759c46783539d00c
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-12 09:47:42 +0600

    Create createTicket.js

---
+++ src/commands/tickets/createTicket.js
@@ -0,0 +1,27 @@
+// src/commands/tickets/createTicket.js
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const Ticket = require('../../models/Ticket');
+const TicketSettings = require('../../models/TicketSettings');
+
+module.exports = {
+  data: {
+    name: 'create-ticket-message',
+    description: 'Send the ticket creation message with buttons',
+  },
+  async execute(interaction) {
+    const ticketRow = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId('create_ticket')
+        .setLabel('Create Ticket')
+        .setStyle('PRIMARY')
+    );
+
+    const embed = new MessageEmbed()
+      .setColor('BLUE')
+      .setTitle('Support Ticket')
+      .setDescription('Click the button below to create a support ticket.');
+
+    await interaction.channel.send({ embeds: [embed], components: [ticketRow] });
+    return interaction.reply({ content: 'Ticket message sent!', ephemeral: true });
+  },
+};
