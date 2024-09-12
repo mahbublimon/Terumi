ef2e357d3cdc51d604a4f3a591a52369f292d0a3
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-12 23:21:56 +0600

    Update database.js

--- database.js
+++ database.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
-// MongoDB connection function
 const connectDB = async () => {
   try {
-    // Connect to MongoDB using the URI from environment variables
+    // Fix deprecation warning
+    mongoose.set('strictQuery', true);
+
+    // Connect to MongoDB
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log('Successfully connected to MongoDB');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
