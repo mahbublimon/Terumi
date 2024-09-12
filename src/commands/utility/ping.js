05e8e81064a8ec2dabd02d284babf6c35d1c42e6
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-13 02:19:11 +0600

    Update ping.js

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,7 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
-    name: 'ping',
-    description: 'Replies with Pong!',
-    async execute(interaction, client) {
-        await interaction.reply('Pong!');
-    }
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
+
+  async execute(interaction) {
+    await interaction.reply('Pong!');
+  },
 };
