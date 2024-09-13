e5ac6a364b839ffad0126ff62d6d244dfd6a276a
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-13 09:34:02 +0600

    Update musicPlayer.js

--- src/utils/musicPlayer.js
+++ src/utils/musicPlayer.js
@@ -1,19 +1,20 @@
 const { Player } = require('discord-player');
-const { client } = require('../../index'); // Make sure the path to the client is correct
+const { client } = require('../bot'); // Correct path to the client
 
-// Initialize the player with correct options
+// Initialize the player with proper options
 const player = new Player(client, {
   ytdlOptions: {
     quality: 'highestaudio',
-    highWaterMark: 1 << 25
-  }
+    highWaterMark: 1 << 25, // Adjusts buffer size to handle larger streams
+  },
 });
 
-// Listen for track events
+// Event: When a track starts playing
 player.on('trackStart', (queue, track) => {
   queue.metadata.channel.send(`🎶 Now playing: **${track.title}**`);
 });
 
+// Event: When the queue has ended
 player.on('queueEnd', (queue) => {
   queue.metadata.channel.send('The queue has ended.');
 });
