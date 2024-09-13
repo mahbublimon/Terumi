b94d3077ee77509b0f2f2986a06a3f6befcc1e0b
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-14 04:28:47 +0600

    Update dashboard.js

--- src/commands/server/dashboard.js
+++ src/commands/server/dashboard.js
@@ -1,15 +1,14 @@
-// src/commands/server/dashboard.js
-const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  data: {
-    name: 'dashboard',
-    description: 'Get a direct link to your server dashboard',
-  },
+  data: new SlashCommandBuilder()
+    .setName('dashboard')
+    .setDescription('Get a direct link to your server dashboard'),
+
   async execute(interaction) {
     const dashboardLink = `http://yourdashboardurl.com/${interaction.guild.id}`;
 
-    const embed = new MessageEmbed()
+    const embed = new EmbedBuilder()
       .setColor('BLUE')
       .setTitle('Server Dashboard')
       .setDescription(`Here is your dashboard link: [Dashboard](${dashboardLink})`)
