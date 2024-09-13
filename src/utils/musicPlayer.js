81604c41cab4d17bd6c77972cc7a6751981140a3
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-13 08:57:56 +0600

    Update musicPlayer.js

--- src/utils/musicPlayer.js
+++ src/utils/musicPlayer.js
@@ -1,7 +1,16 @@
-// src/utils/musicPlayer.js
 const { Player } = require('discord-player');
+const { client } = require('../../index'); // Import the client
 
-// Initialize the music player
-const player = new Player();
+// Create a new player instance
+const player = new Player(client);
+
+// Optionally handle events (e.g., track start, queue end)
+player.on('trackStart', (queue, track) => {
+  queue.metadata.channel.send(`🎶 Now playing **${track.title}**!`);
+});
+
+player.on('queueEnd', (queue) => {
+  queue.metadata.channel.send('The queue has ended.');
+});
 
 module.exports = player;
