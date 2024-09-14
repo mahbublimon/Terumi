5e4ed3f2825773d5a22d4d6bc0ac565aea2a62d8
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-14 16:51:40 +0600

    Update closeTicket.js

--- src/commands/tickets/closeTicket.js
+++ src/commands/tickets/closeTicket.js
@@ -1,19 +1,15 @@
-// src/commands/tickets/closeTicket.js
+const { SlashCommandBuilder } = require('discord.js');
 const ticketHandler = require('../../utils/ticketHandler');
 
 module.exports = {
-  data: {
-    name: 'close-ticket',
-    description: 'Close a support ticket',
-    options: [
-      {
-        name: 'ticket_id',
-        type: 'STRING',
-        description: 'The ID of the ticket to close',
-        required: true,
-      },
-    ],
-  },
+  data: new SlashCommandBuilder()
+    .setName('close-ticket')
+    .setDescription('Close a support ticket')
+    .addStringOption(option => 
+      option.setName('ticket_id')
+        .setDescription('The ID of the ticket to close')
+        .setRequired(true)
+    ),
   async execute(interaction) {
     const ticketID = interaction.options.getString('ticket_id');
     await ticketHandler.closeTicket(interaction, ticketID);
