cc75e021dd1d724d835f62b7a11de2b4716ec669
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-14 16:50:29 +0600

    Update resetData.js

--- src/commands/server/resetData.js
+++ src/commands/server/resetData.js
@@ -1,24 +1,19 @@
-// src/commands/server/resetData.js
-const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const User = require('../../models/User');
 
 module.exports = {
-  data: {
-    name: 'reset',
-    description: 'Reset user data like levels or credits',
-    options: [
-      {
-        name: 'data_type',
-        type: 'STRING',
-        description: 'The type of data to reset (e.g., levels, credits)',
-        required: true,
-        choices: [
+  data: new SlashCommandBuilder()
+    .setName('reset')
+    .setDescription('Reset user data like levels or credits')
+    .addStringOption(option => 
+      option.setName('data_type')
+        .setDescription('The type of data to reset (e.g., levels, credits)')
+        .setRequired(true)
+        .addChoices(
           { name: 'levels', value: 'levels' },
-          { name: 'credits', value: 'credits' },
-        ],
-      },
-    ],
-  },
+          { name: 'credits', value: 'credits' }
+        )
+    ),
   async execute(interaction) {
     const dataType = interaction.options.getString('data_type');
     const users = await User.find({ guildID: interaction.guild.id });
@@ -32,7 +27,7 @@ module.exports = {
       await user.save();
     }
 
-    const embed = new MessageEmbed()
+    const embed = new EmbedBuilder()
       .setColor('RED')
       .setTitle('Data Reset')
       .setDescription(`All users' **${dataType}** have been reset!`)
