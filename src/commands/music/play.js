1b7dfdb08973e6537ad1988cc18f23cf22ea33bf
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-15 15:03:14 +0600

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,6 +1,6 @@
-const { SlashCommandBuilder } = require('@discordjs/builders'); // Add this import
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { QueryType } = require('discord-player');
-const player = require('../../utils/musicPlayer');
+const player = require('../../utils/musicPlayer'); // Ensure player is properly initialized in this file
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
@@ -17,10 +17,12 @@ module.exports = {
     const query = interaction.options.getString('query');
     const voiceChannel = interaction.member.voice.channel;
 
+    // Check if the user is in a voice channel
     if (!voiceChannel) {
       return interaction.reply({ content: 'You must be in a voice channel to play music!', ephemeral: true });
     }
 
+    // Create the queue for the guild
     const queue = player.createQueue(interaction.guild, {
       metadata: {
         channel: interaction.channel,
@@ -28,29 +30,35 @@ module.exports = {
     });
 
     try {
+      // Connect to the voice channel if not already connected
       if (!queue.connection) await queue.connect(voiceChannel);
-    } catch {
+    } catch (err) {
       queue.destroy();
+      console.error(err); // Log the error to track issues
       return interaction.reply({ content: 'Could not join your voice channel!', ephemeral: true });
     }
 
+    // Search for the song using the query
     const track = await player.search(query, {
       requestedBy: interaction.user,
       searchEngine: QueryType.AUTO,
     }).then(x => x.tracks[0]);
 
+    // If no track is found
     if (!track) {
       return interaction.reply({ content: `No results found for ${query}!`, ephemeral: true });
     }
 
+    // Add the track to the queue
     queue.addTrack(track);
     if (!queue.playing) await queue.play();
 
+    // Create an embed to show the now playing track
     const playEmbed = new EmbedBuilder()
       .setTitle('🎶 Now Playing')
-      .setDescription(`**${track.title}**`)
+      .setDescription(`**[${track.title}](${track.url})**`)
       .setURL(track.url)
-      .setColor('GREEN');
+      .setColor('#1DB954') // Use Spotify Green Hex Code for color
 
     return interaction.reply({ embeds: [playEmbed] });
   },
