1a112e15993c8c7f439110f39854ba7807f23ade
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-16 18:49:12 +0600

    Update seek.js

--- src/commands/music/seek.js
+++ src/commands/music/seek.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders'); // Add this import
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const player = require('../../utils/musicPlayer');
 
 module.exports = {
@@ -16,7 +16,7 @@ module.exports = {
     const time = interaction.options.getString('time');
 
     if (!queue || !queue.playing) {
-      return interaction.reply({ content: 'There is no music playing!', ephemeral: true });
+      return interaction.reply({ content: 'There is no music playing to seek!', ephemeral: true });
     }
 
     const [minutes, seconds] = time.split(':').map(Number);
