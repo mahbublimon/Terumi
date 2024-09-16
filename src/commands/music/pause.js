0e9a006d649c4c80eca7a9e64be5ccb60ef89700
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-16 18:40:03 +0600

    Update pause.js

--- src/commands/music/pause.js
+++ src/commands/music/pause.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders'); // Add this import
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const player = require('../../utils/musicPlayer');
 
 module.exports = {
