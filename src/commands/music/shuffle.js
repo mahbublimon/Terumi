9bf97e14401dd44ca63fe8c966281d6fa84a41e9
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-16 18:49:45 +0600

    Update shuffle.js

--- src/commands/music/shuffle.js
+++ src/commands/music/shuffle.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders'); // Add this import
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const player = require('../../utils/musicPlayer');
 
 module.exports = {
@@ -10,7 +10,7 @@ module.exports = {
     const queue = player.getQueue(interaction.guild.id);
 
     if (!queue || !queue.playing) {
-      return interaction.reply({ content: 'There is no music playing!', ephemeral: true });
+      return interaction.reply({ content: 'There is no music playing to shuffle!', ephemeral: true });
     }
 
     queue.shuffle();
