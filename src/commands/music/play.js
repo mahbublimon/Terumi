ce8d16db7cc52baa1093162701514440a0b0af9b
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-17 01:02:42 +0600

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,39 +1,32 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const musicPlayer = require('../../utils/musicPlayer'); // Updated import
-const { EmbedBuilder } = require('discord.js');
+const { searchSpotifyTrack, playSpotifyTrack } = require('../../utils/musicPlayer');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('play')
-    .setDescription('Play a song from Spotify')
-    .addStringOption(option =>
-      option.setName('query')
-        .setDescription('Song title or URL')
-        .setRequired(true)
-    ),
+    data: new SlashCommandBuilder()
+        .setName('play')
+        .setDescription('Play a song from Spotify')
+        .addStringOption(option =>
+            option.setName('query')
+                .setDescription('The name of the song or artist')
+                .setRequired(true)
+        ),
 
-  async execute(interaction) {
-    const query = interaction.options.getString('query');
-    const voiceChannel = interaction.member.voice.channel;
+    async execute(interaction) {
+        const query = interaction.options.getString('query');
 
-    if (!voiceChannel) {
-      return interaction.reply({ content: 'You must be in a voice channel to play music!', ephemeral: true });
+        try {
+            // Search for the track on Spotify
+            const trackInfo = await searchSpotifyTrack(query);
+
+            if (!trackInfo) {
+                return interaction.reply({ content: 'No results found on Spotify for your query!', ephemeral: true });
+            }
+
+            // Play the track in the user's voice channel
+            await playSpotifyTrack(interaction, trackInfo);
+        } catch (error) {
+            console.error('Error playing music:', error);
+            await interaction.reply({ content: 'An error occurred while trying to play the music.', ephemeral: true });
+        }
     }
-
-    try {
-      // Use Spotify API to search for the track
-      const track = await musicPlayer.searchSpotifyTrack(query);
-
-      if (!track) {
-        return interaction.reply({ content: `No results found for ${query}!`, ephemeral: true });
-      }
-
-      // Call the play function
-      const trackUrl = track.external_urls.spotify; // Spotify URL for the track
-      await musicPlayer.play(interaction, trackUrl, track); // Pass track information for playback
-    } catch (error) {
-      console.error('Error playing music:', error);
-      return interaction.reply({ content: 'There was an error executing this command!', ephemeral: true });
-    }
-  },
 };
