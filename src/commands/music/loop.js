2e3add0b16a6a771444a1b779c2c6796523d27e5
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-16 18:48:30 +0600

    Update loop.js

--- src/commands/music/loop.js
+++ src/commands/music/loop.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders'); // Import SlashCommandBuilder
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const player = require('../../utils/musicPlayer');
 
 module.exports = {
@@ -7,31 +7,32 @@ module.exports = {
     .setDescription('Toggle loop mode for the current song or the entire queue')
     .addStringOption(option =>
       option.setName('mode')
-        .setDescription('Loop mode: track, queue, or off')
+        .setDescription('Repeat mode: track, queue, or off')
         .setRequired(true)
         .addChoices(
           { name: 'Track', value: 'track' },
           { name: 'Queue', value: 'queue' },
           { name: 'Off', value: 'off' }
-        )),
+        )
+    ),
 
   async execute(interaction) {
     const queue = player.getQueue(interaction.guild.id);
     const mode = interaction.options.getString('mode');
 
     if (!queue || !queue.playing) {
-      return interaction.reply({ content: 'There is no music playing!', ephemeral: true });
+      return interaction.reply({ content: 'There is no music playing to loop!', ephemeral: true });
     }
 
     if (mode === 'track') {
       queue.setRepeatMode(1); // Repeat track
-      return interaction.reply({ content: '🔂 Loop mode set to: Track' });
+      return interaction.reply({ content: '🔂 Repeat mode set to: Track' });
     } else if (mode === 'queue') {
       queue.setRepeatMode(2); // Repeat queue
-      return interaction.reply({ content: '🔁 Loop mode set to: Queue' });
+      return interaction.reply({ content: '🔁 Repeat mode set to: Queue' });
     } else {
-      queue.setRepeatMode(0); // Disable loop
-      return interaction.reply({ content: '🔁 Loop mode turned off' });
+      queue.setRepeatMode(0); // Disable repeat
+      return interaction.reply({ content: '🔁 Repeat mode turned off' });
     }
   },
 };
