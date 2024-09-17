c1bf33318a4ffb1cf7ade6957e347beeb66f84cd
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-17 11:26:08 +0600

    Update spotifyClient.js

--- src/utils/spotifyClient.js
+++ src/utils/spotifyClient.js
@@ -1,20 +1,20 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 
+// Create Spotify client
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
 });
 
-// Retrieve an access token
-async function refreshSpotifyToken() {
-    try {
-        const data = await spotifyApi.clientCredentialsGrant();
-        spotifyApi.setAccessToken(data.body['access_token']);
-        console.log('Spotify access token retrieved successfully');
-    } catch (error) {
-        console.error('Error retrieving Spotify access token', error);
-    }
-}
+const refreshSpotifyToken = async () => {
+  try {
+    const data = await spotifyApi.clientCredentialsGrant();
+    spotifyApi.setAccessToken(data.body.access_token);
+    console.log('Spotify access token refreshed');
+  } catch (error) {
+    console.error('Error retrieving Spotify access token:', error);
+  }
+};
 
 // Refresh token every hour
 setInterval(refreshSpotifyToken, 3600 * 1000);
