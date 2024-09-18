98322d902edd5ea453ee60f1f82d089213bc3e0b
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-18 22:12:33 +0600

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,44 +1,47 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 const { searchSpotifyTrack } = require('../../utils/spotifyClient');
-const { playSpotifyTrack } = require('../../utils/musicPlayer');
+const { playFullSong } = require('../../utils/musicPlayer');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('play')
-    .setDescription('Play a song from Spotify or another source')
-    .addStringOption(option => 
+    .setDescription('Play a song by searching on Spotify')
+    .addStringOption(option =>
       option.setName('query')
         .setDescription('Song title or Spotify URL')
-        .setRequired(true)
-    ),
-
+        .setRequired(true)),
+  
   async execute(interaction) {
+    await interaction.deferReply(); // Allow time for searching the track
+
     const query = interaction.options.getString('query');
+    const track = await searchSpotifyTrack(query);
+
+    if (!track) {
+      return interaction.editReply(`No results found for "${query}".`);
+    }
 
     try {
-      // Defer reply to allow the bot time to search and play the track
-      await interaction.deferReply();
+      // Play the full song
+      await playFullSong(interaction, track);
 
-      // Search for the track on Spotify
-      const track = await searchSpotifyTrack(query);
-      if (!track) {
-        return interaction.editReply(`No results found for "${query}"!`);
-      }
+      // Create an embedded message
+      const embed = new MessageEmbed()
+        .setColor('#1DB954') // Spotify green color
+        .setTitle(`Now Playing: ${track.name}`)
+        .setURL(track.external_urls.spotify) // Spotify track URL
+        .setDescription(`**Artist:** ${track.artists.map(artist => artist.name).join(', ')}\n**Album:** ${track.album.name}`)
+        .setThumbnail(track.album.images[0]?.url || '') // Album cover image
+        .setFooter('Enjoy your music!', 'https://cdn-icons-png.flaticon.com/512/174/174872.png') // Spotify logo in footer
+        .setTimestamp();
 
-      // Play the track using playSpotifyTrack (handles playing preview or fallback to YouTube)
-      await playSpotifyTrack(interaction, track);
+      // Send the embed
+      interaction.editReply({ embeds: [embed] });
 
-      // Edit the deferred reply with success message
-      await interaction.editReply(`Now playing: **${track.name}** by ${track.artists.map(a => a.name).join(', ')}`);
     } catch (error) {
       console.error('Error playing the track:', error);
-
-      // Handle errors gracefully and reply if not already done
-      if (interaction.deferred) {
-        await interaction.editReply('There was an error playing the track.');
-      } else if (!interaction.replied) {
-        await interaction.reply('There was an error playing the track.');
-      }
+      interaction.editReply('There was an error playing the track.');
     }
   },
 };
