c88321da9933fd744f73ddabbe4b5e239bc832c2
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-18 14:57:39 +0600

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,35 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { searchSpotifyTrack, playSpotifyTrack } = require('../../utils/musicPlayer');
+const { searchSpotifyTrack } = require('../../utils/spotifyClient');
+const { playSpotifyTrack } = require('../../utils/musicPlayer');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('play')
     .setDescription('Play a song from Spotify')
     .addStringOption(option => 
-      option.setName('query')
-        .setDescription('Song title or Spotify URL')
-        .setRequired(true)
-    ),
+      option.setName('query').setDescription('Song title or Spotify URL').setRequired(true)),
   
   async execute(interaction) {
     const query = interaction.options.getString('query');
-
-    // Search for the track using the query
     const track = await searchSpotifyTrack(query);
 
     if (!track) {
-      return interaction.reply({ content: `No results found for "${query}"!`, ephemeral: true });
+      return interaction.reply(`No results found for "${query}"!`);
     }
 
     try {
-      // Play the track in the voice channel using the preview URL
       await playSpotifyTrack(interaction, track.preview_url);
-
-      // Send a reply to let the user know the track is playing
-      await interaction.reply({ content: `Now playing: **${track.name}** by ${track.artists.map(artist => artist.name).join(', ')}` });
+      interaction.reply(`Now playing: ${track.name} by ${track.artists.map(a => a.name).join(', ')}`);
     } catch (error) {
-      console.error('Error playing track:', error);
-      interaction.reply({ content: 'There was an error playing the track!', ephemeral: true });
+      console.error(error);
+      interaction.reply('Error playing the track.');
     }
   },
 };
