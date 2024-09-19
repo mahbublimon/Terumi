8d28f457ef312e8581450de703288a89631b374b
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-20 05:42:52 +0600

    Update mute.js

--- src/commands/moderation/mute.js
+++ src/commands/moderation/mute.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const hasAdminPermissions = require('../../utils/permissionCheck');
-const ms = require('ms');
+const ms = require('ms'); // For time parsing
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -27,17 +27,36 @@ module.exports = {
     const user = interaction.options.getUser('user');
     const duration = interaction.options.getString('duration');
     const reason = interaction.options.getString('reason');
-    const muteRole = interaction.guild.roles.cache.find(role => role.name === 'Muted');
+    let muteRole = interaction.guild.roles.cache.find(role => role.name === 'Muted');
 
+    // Create the "Muted" role if it doesn't exist
     if (!muteRole) {
-      return interaction.reply('Mute role not found. Please create a role called "Muted".');
+      try {
+        muteRole = await interaction.guild.roles.create({
+          name: 'Muted',
+          color: '#555555',
+          permissions: []
+        });
+
+        // Disable SEND_MESSAGES and ADD_REACTIONS for the Muted role in all channels
+        interaction.guild.channels.cache.forEach(async (channel) => {
+          await channel.permissionOverwrites.edit(muteRole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      } catch (error) {
+        console.error('Error creating Muted role:', error);
+        return interaction.reply({ content: 'There was an error creating the Muted role.', ephemeral: true });
+      }
     }
 
     const member = interaction.guild.members.cache.get(user.id);
     if (member.roles.cache.has(muteRole.id)) {
-      return interaction.reply('User is already muted.');
+      return interaction.reply({ content: 'User is already muted.', ephemeral: true });
     }
 
+    // Add the Muted role to the user
     await member.roles.add(muteRole);
 
     const embed = new EmbedBuilder()
@@ -51,16 +70,17 @@ module.exports = {
 
     const replyMessage = await interaction.reply({ embeds: [embed], fetchReply: true });
 
-    setTimeout(() => {
-      interaction.deleteReply().catch(console.error);
-    }, 10000);
-
-    // Unmute after the specified duration
+    // Automatically unmute after the specified duration
     setTimeout(async () => {
       if (member.roles.cache.has(muteRole.id)) {
         await member.roles.remove(muteRole);
         await member.send('You have been unmuted.');
       }
     }, ms(duration));
+
+    // Delete the command message after 10 seconds
+    setTimeout(() => {
+      interaction.deleteReply().catch(console.error);
+    }, 10000);
   },
 };
