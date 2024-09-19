cee9ddcaa4a5395bdbeb18cbf07020b733f2b590
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-19 16:21:31 +0600

    Update vote.js

--- src/commands/misc/vote.js
+++ src/commands/misc/vote.js
@@ -9,7 +9,7 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setColor('PURPLE')
       .setTitle('Vote for Terumi')
-      .setDescription('Support Terumi by voting for us! [Click here to vote](https://top.gg/bot/your-bot-id)')
+      .setDescription('Support Terumi by voting for us! [Click here to vote](https://top.gg/bot/)')
       .setTimestamp();
 
     return interaction.reply({ embeds: [embed] });
