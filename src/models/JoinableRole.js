f0d2383e3face98e0007833f32ea2a3d9e8ff44b
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-20 07:08:35 +0600

    Create JoinableRole.js

---
+++ src/models/JoinableRole.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const joinableRoleSchema = new mongoose.Schema({
+  guildID: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  roleID: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('JoinableRole', joinableRoleSchema);
