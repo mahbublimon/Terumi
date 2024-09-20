24a75cf16d825ae0786f025b83d55d8a1075d45f
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-20 12:57:13 +0600

    Update vote.js

--- src/commands/misc/vote.js
+++ src/commands/misc/vote.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, Colors } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,7 +7,7 @@ module.exports = {
 
   async execute(interaction) {
     const embed = new EmbedBuilder()
-      .setColor(Colors.Purple) // Use the correct color constant
+      .setColor(0x800080) // Purple in hexadecimal (you can also use other colors like EmbedBuilder.Colors.Blue)
       .setTitle('Vote for Terumi')
       .setDescription('Support Terumi by voting for us! [Click here to vote](https://top.gg/bot/1282770910197190666/vote)')
       .setTimestamp();
