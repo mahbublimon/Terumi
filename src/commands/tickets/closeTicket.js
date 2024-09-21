ca60d981f9f3617257f64388a335f55e54442785
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-21 15:53:53 +0600

    Update closeTicket.js

--- src/commands/tickets/closeTicket.js
+++ src/commands/tickets/closeTicket.js
@@ -1,17 +1,30 @@
 const { SlashCommandBuilder } = require('discord.js');
-const ticketHandler = require('../../utils/ticketHandler');
+const Ticket = require('../../models/Ticket');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('close-ticket')
-    .setDescription('Close a support ticket')
+    .setDescription('Close an existing ticket')
     .addStringOption(option => 
-      option.setName('ticket_id')
+      option.setName('ticket-id')
         .setDescription('The ID of the ticket to close')
         .setRequired(true)
     ),
+
   async execute(interaction) {
-    const ticketID = interaction.options.getString('ticket_id');
-    await ticketHandler.closeTicket(interaction, ticketID);
+    const ticketId = interaction.options.getString('ticket-id');
+    const ticket = await Ticket.findOne({ _id: ticketId });
+
+    if (!ticket) {
+      return interaction.reply({ content: 'Ticket not found.', ephemeral: true });
+    }
+
+    // Close the ticket and archive it
+    await Ticket.updateOne({ _id: ticketId }, { status: 'closed' });
+
+    const ticketChannel = interaction.guild.channels.cache.get(ticket.channelId);
+    if (ticketChannel) await ticketChannel.delete();
+
+    await interaction.reply({ content: `Ticket #${ticketId} has been closed and archived.`, ephemeral: true });
   },
 };
