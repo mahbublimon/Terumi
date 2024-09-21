d7e8823416db6800f9c4135b759e9a2be0fa79f9
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-21 19:11:54 +0600

    Update rerollTicket.js

--- src/commands/tickets/rerollTicket.js
+++ src/commands/tickets/rerollTicket.js
@@ -1,40 +1,48 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const Ticket = require('../../models/Ticket');
-const autoAssign = require('../../utils/autoAssign');
+const { assignSupport, releaseSupport } = require('../../utils/autoAssign');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('reroll-ticket')
-    .setDescription('Reroll the ticket to another support staff'),
-
+    .setDescription('Reassign the ticket to a different support member')
+    .addChannelOption(option =>
+      option.setName('channel')
+        .setDescription('The ticket channel to reroll')
+        .setRequired(true)
+    ),
+  
   async execute(interaction) {
-    const channel = interaction.channel;
-
+    const channel = interaction.options.getChannel('channel');
     const ticket = await Ticket.findOne({ channelID: channel.id });
-    
+
     if (!ticket) {
       return interaction.reply({ content: 'This is not a valid ticket channel.', ephemeral: true });
     }
 
-    // Assign a new support staff
-    const newSupport = await autoAssign.assignSupport();
+    // Release the previous support member
+    await releaseSupport(ticket.supportID);
 
-    // Update the ticket with the new support staff
-    ticket.supportID = newSupport.id;
+    // Assign a new support member
+    let newSupport;
+    try {
+      newSupport = await assignSupport();
+    } catch (error) {
+      return interaction.reply({ content: 'No available support staff to reroll.', ephemeral: true });
+    }
+
+    // Update ticket with the new support member
+    ticket.supportID = newSupport.userID;
     await ticket.save();
 
-    // Update channel permissions to reflect the new support staff
-    await channel.permissionOverwrites.edit(newSupport.id, {
-      VIEW_CHANNEL: true,
-      SEND_MESSAGES: true,
-    });
-
-    // Notify the channel
+    // Notify about the reroll
     const embed = new EmbedBuilder()
-      .setTitle(`Ticket #${ticket.id} Rerolled`)
-      .setDescription(`The ticket has been reassigned to ${newSupport.tag}.`)
-      .setColor('YELLOW');
+      .setColor('GREEN')
+      .setTitle('Ticket Rerolled')
+      .setDescription(`This ticket has been reassigned to <@${newSupport.userID}>.`)
+      .setTimestamp();
 
+    await interaction.reply({ embeds: [embed], ephemeral: true });
     await channel.send({ embeds: [embed] });
-  }
+  },
 };
