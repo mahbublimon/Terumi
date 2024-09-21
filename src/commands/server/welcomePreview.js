c28cd89e480c6179da4447af44cb6035fb79842e
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-21 11:09:47 +0600

    Update welcomePreview.js

--- src/commands/server/welcomePreview.js
+++ src/commands/server/welcomePreview.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const WelcomeConfig = require('../../models/WelcomeConfig'); // Assuming MongoDB schema
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('welcome-preview') // Changed command name to be unique
-    .setDescription('Preview the welcome message for a user')
+    .setName('welcome-preview')
+    .setDescription('Preview the current welcome message')
     .addUserOption(option =>
       option.setName('user')
         .setDescription('The user to preview the welcome message for')
@@ -12,13 +13,19 @@ module.exports = {
   
   async execute(interaction) {
     const user = interaction.options.getUser('user');
+    const welcomeConfig = await WelcomeConfig.findOne({ guildId: interaction.guild.id });
+
+    if (!welcomeConfig) {
+      return interaction.reply({ content: 'No welcome message configuration found.', ephemeral: true });
+    }
+
     const embed = new EmbedBuilder()
       .setColor('YELLOW')
-      .setTitle('Welcome Message Preview')
-      .setDescription(`Welcome, **${user.username}** to the server! We hope you enjoy your stay! 🎉`)
-      .setThumbnail(user.displayAvatarURL())
+      .setTitle(welcomeConfig.title || 'Welcome!')
+      .setDescription(welcomeConfig.description.replace('{{username}}', user.username))
+      .setThumbnail(welcomeConfig.thumbnail || user.displayAvatarURL())
       .setTimestamp();
 
-    return interaction.reply({ embeds: [embed] });
+    return interaction.reply({ embeds: [embed], ephemeral: true });
   },
 };
