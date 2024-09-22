9c32205f7b8507232d712a5a8fa69f6db4df4971
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-22 10:20:08 +0600

    Update messageCounter.js

--- src/utils/messageCounter.js
+++ src/utils/messageCounter.js
@@ -1,24 +1,26 @@
-let messagesPerMinute = 0;
 let messageCount = 0;
+let messagesPerMinute = 0;
 
-// Increment message count when a new message is created
+// Increment message count whenever a message is created
 function incrementMessageCount() {
   messageCount++;
 }
 
-// Reset message count and calculate messages per minute
-function resetMessageCount() {
+// Calculate messages per minute and reset the counter
+function calculateMessagesPerMinute() {
   messagesPerMinute = messageCount;
-  messageCount = 0;
+  messageCount = 0; // Reset the message count every minute
 }
 
-// Get the current messages per minute
+// Get the current messages per minute value
 function getMessagesPerMinute() {
   return messagesPerMinute;
 }
 
+// Set interval to calculate messages per minute every 60 seconds
+setInterval(calculateMessagesPerMinute, 60000);
+
 module.exports = {
   incrementMessageCount,
-  resetMessageCount,
   getMessagesPerMinute,
 };
