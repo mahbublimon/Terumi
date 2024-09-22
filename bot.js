7f426663e846311662ae2f65a0551002a3a00c19
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-22 10:20:57 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { incrementMessageCount } = require('./src/utils/messageCounter'); // Import the message counter
 
 // Initialize Discord Client
 const client = new Client({
@@ -9,6 +10,14 @@ const client = new Client({
     GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.GuildMembers,
   ],
+});
+
+// Listen to the messageCreate event to increment the message count
+client.on('messageCreate', (message) => {
+  // Ignore bot messages
+  if (!message.author.bot) {
+    incrementMessageCount(); // Increment message count
+  }
 });
 
 // Initialize command collection
