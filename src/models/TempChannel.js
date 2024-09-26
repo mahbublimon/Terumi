948d0001197b42824d9af057e74262ce93186e08
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-27 04:43:23 +0600

    Update and rename TemporaryChannelSettings.js to TempChannel.js

---
+++ src/models/TempChannel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const tempChannelSchema = new mongoose.Schema({
+  guildID: { type: String, required: true },
+  channelID: { type: String, required: true },
+});
+
+module.exports = mongoose.model('TempChannel', tempChannelSchema);
