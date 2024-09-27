68e5b54b1e92303d83fde760b4ffcb0087f33971
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-09-27 11:35:15 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,22 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, ShardingManager } = require('discord.js');
 const { incrementMessageCount } = require('./src/utils/messageCounter'); // Import the message counter
+require('dotenv').config();
 
-// Initialize Discord Client
+// Initialize Sharding Manager
+const manager = new ShardingManager('./bot.js', {
+  token: process.env.TOKEN,
+  totalShards: 'auto', // This will spawn the number of shards based on Discord's recommendation
+});
+
+// Spawn the shards
+manager.spawn();
+
+// Log when a shard is launched
+manager.on('shardCreate', shard => {
+  console.log(`Launched shard ${shard.id}`);
+});
+
+// Discord Client
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -14,7 +29,6 @@ const client = new Client({
 
 // Listen to the messageCreate event to increment the message count
 client.on('messageCreate', (message) => {
-  // Ignore bot messages
   if (!message.author.bot) {
     incrementMessageCount(); // Increment message count
   }
@@ -23,5 +37,11 @@ client.on('messageCreate', (message) => {
 // Initialize command collection
 client.commands = new Collection();
 
-// Export the client for use in other parts of the application
+client.once('ready', () => {
+  console.log(`${client.user.tag} is online and ready!`);
+});
+
+// Login the client
+client.login(process.env.TOKEN);
+
 module.exports = { client };
